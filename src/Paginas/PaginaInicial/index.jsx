34d1f092cee505044bd0f6c92764b4ccf1315647
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-23 09:37:50 -0300

    Pagina inicial: estilos e aplicação do banner

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -1,8 +1,17 @@
 import Cabecalho from "../../Components/Cabecalho";
+import Menu from '../../Components/Menu';
+import Banner from "../../Components/Banner";
+import styles from './PaginaInicial.module.scss';
 
 function PaginaInicial(){
   return (
-    <Cabecalho />
+    <>
+      <Cabecalho />
+      <div className={styles.principal}>
+        <Menu />
+        <Banner />
+      </div>
+    </>
   );
 }
 
