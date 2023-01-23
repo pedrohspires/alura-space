d4dffa32d91c429b953df0d5d9192ae64ad46205
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-23 10:30:37 -0300

    Criando e adicionando rodapé na página inicial

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -2,15 +2,19 @@ import Cabecalho from "../../Components/Cabecalho";
 import Menu from '../../Components/Menu';
 import Banner from "../../Components/Banner";
 import styles from './PaginaInicial.module.scss';
+import Rodape from "../../Components/Rodape";
 
 function PaginaInicial(){
   return (
     <>
       <Cabecalho />
-      <div className={styles.principal}>
-        <Menu />
-        <Banner />
-      </div>
+      <main>
+        <div className={styles.principal}>
+          <Menu />
+          <Banner />
+        </div>
+      </main>
+      <Rodape />
     </>
   );
 }
