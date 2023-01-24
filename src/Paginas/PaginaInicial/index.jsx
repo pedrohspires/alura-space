e1a31d9d41ba26878a68439fbf680cd30c6d2e4a
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-24 09:30:07 -0300

    Pagina principal com as fotos da galeria, menu e populares

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -3,6 +3,8 @@ import Menu from '../../Components/Menu';
 import Banner from "../../Components/Banner";
 import styles from './PaginaInicial.module.scss';
 import Rodape from "../../Components/Rodape";
+import Galeria from "../../Components/Galeria";
+import Populares from "../../Components/Populares";
 
 function PaginaInicial(){
   return (
@@ -12,6 +14,10 @@ function PaginaInicial(){
         <div className={styles.principal}>
           <Menu />
           <Banner />
+        </div>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
         </div>
       </main>
       <Rodape />
