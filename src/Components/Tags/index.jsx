144590fd74814605485c4cabf5bd4444249ea33e
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-24 09:29:33 -0300

    Tags da galeria

---
+++ src/Components/Tags/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Tags.module.scss';
+
+function Tags() {
+  return (
+    <div className={styles.tags}>
+      <p>Filtre por tags</p>
+      <ul className={styles.tags__lista}>
+        <li>Estrelas</li>
+        <li>Galáxias</li>
+        <li>Lua</li>
+        <li>Planetas</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Tags
