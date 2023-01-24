92be6f28ab77fedc4041b8c21d0e2adeb58553bd
Pedro Henrique Sousa Pires <pedropires2603@gmail.com>
2023-01-24 11:19:12 -0300

    Absolute import

--- src/Paginas/PaginaInicial/index.jsx
+++ src/Paginas/PaginaInicial/index.jsx
@@ -1,10 +1,10 @@
-import Cabecalho from "../../Components/Cabecalho";
-import Menu from '../../Components/Menu';
-import Banner from "../../Components/Banner";
-import styles from './PaginaInicial.module.scss';
-import Rodape from "../../Components/Rodape";
-import Galeria from "../../Components/Galeria";
-import Populares from "../../Components/Populares";
+import Cabecalho from "Components/Cabecalho";
+import Menu from "Components/Menu";
+import Banner from "Components/Banner";
+import styles from "./PaginaInicial.module.scss";
+import Rodape from "Components/Rodape";
+import Galeria from "Components/Galeria";
+import Populares from "Components/Populares";
 
 function PaginaInicial(){
   return (
